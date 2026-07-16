e32c1f08c95fef730eaa9bb7ab69187ffd502094
laijin <lajin8078@gmail.com>
2026-07-16 12:38:26 +0800

    Fix images on Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders,passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ljin.top',
+	image: {
+    service: passthroughImageService(),
+	},
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
